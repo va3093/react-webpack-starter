0369e835996b5c6e3e6caf5277149152b9988929
Wilhelm <va3093@gmail.com>
2017-01-24 21:50:39 +0000

    renamed example directory

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ var config = {
   output: {
     path: __dirname + '/lib',
     filename: outputFile,
-    publicPath: __dirname + '/example'
+    publicPath: __dirname + '/public'
   },
   module: {
     loaders: [
@@ -51,7 +51,7 @@ var config = {
 
 if (env === 'dev') {
   new WebpackDevServer(webpack(config), {
-    contentBase: './example',
+    contentBase: './public',
     hot: true,
     debug: true
   }).listen(port, host, function (err, result) {
